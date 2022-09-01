693726d714ce5c4c3e8793e5b75520f483dc62d9
Marcus Wu <102217370+WunderleMarcus@users.noreply.github.com>
2022-09-01 07:02:19 +0200

    Add files via upload

---
+++ goginframework/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+const express = require("express")
+const cors = require("cors")
+const csv = require("csv-parser")
+const fs = require("fs")
+const ObjectToCsv = require("objects-to-csv")
+
+func main() {
+	fmt.Println("")
+}
